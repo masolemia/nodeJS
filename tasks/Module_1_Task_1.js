646a1634f97fc7e75d2305a642e193f9510593d9
Maria Soldatenko <maria_soldatenko@epam.com>
2021-11-28 16:19:25 +0300

    Module 1 task 1 completed

---
+++ tasks/Module_1_Task_1.js
@@ -0,0 +1,9 @@
+function ask() {
+  process.stdout.write("Enter your word ");
+}
+
+process.stdin.on("data", function (data) {
+  let result = data.toString().split("").reverse().join("");
+  process.stdout.write(`${result}`);
+});
+ask();
