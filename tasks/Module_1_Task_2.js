ad6eaefdf560c8b3abe70da925f22a33ce25693d
Maria Soldatenko <maria_soldatenko@epam.com>
2022-01-20 18:42:52 +0300

    Task 2 still don't understanf

--- tasks/Module_1_Task_2.js
+++ tasks/Module_1_Task_2.js
@@ -1,17 +1,44 @@
 const fs = require("fs");
 const csv = require("csvtojson");
-const csvFilePath = "../files/nodejs-hw1-ex1.csv";
+const csvFilePath = "files/nodejs-hw1-ex1.csv";
+const newFilePath = "files/nodejs-hw1-ex1.txt";
 
 csv()
   .fromFile(csvFilePath)
   .then((jsonObj) => {
+    //let data = JSON.stringify(jsonObj);
     let data = jsonObj.toString();
-    fs.writeFile("../files/nodejs-hw1-ex1.txt", data, (error) => {
+    return data;
+  })
+  .then((data) => {
+    fs.writeFile(newFilePath, data, (error) => {
       if (error) throw error;
       console.log("file is saved");
     });
   });
 
-// fs.readFile("../files/example.txt", "utf-8", (error, file) => {
-//   console.log(file);
+// function convertingFile(csvFilePath, newFilePath, callback) {
+//   fs.readFile(csvFilePath, "utf-8", (error, file) => {
+//     if (error) {
+//       console.error(error);
+//       callback(error);
+//       return;
+//     }
+//     fs.writeFile(newFilePath, file, (error) => {
+//       if (error) {
+//         console.error(error);
+//         callback(error);
+//         return;
+//       }
+//       callback(null);
+//     });
+//   });
+// }
+
+// convertingFile(csvFilePath, newFilePath, (error) => {
+//   if (!error) {
+//     console.log("file was converted");
+//   } else {
+//     console.log("file wasn't converted");
+//   }
 // });
